1dd5f21bf588c35118834edefe8c0e8d91a39ecf
Kasper Mróz <kaspermroz1234@gmail.com>
2019-02-05 20:08:36 +0100

    Configured webpack for prod and dev, added loader for css and sass

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-console.log("Hello world")
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
 
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <h2>Count: {count}</h2>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 });
+          }}
+        >
+          Add
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
