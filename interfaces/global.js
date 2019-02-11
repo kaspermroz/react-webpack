db2d249be6e93ca7d48b28ec00e241bec965fbc1
Kasper Mróz <kaspermroz1234@gmail.com>
2019-02-11 20:42:20 +0100

    Release 1.0.0

--- interfaces/global.js
+++ interfaces/global.js
@@ -1,4 +1,12 @@
 // @flow
+import * as React from 'react'
+import { type } from 'os'
+
 declare module CSSModule {
   declare module.exports: { [key: string]: string }
+}
+
+declare module 'react-axe' {
+  declare function axe(React: any, ReactDOM: any, delay: number): void
+  declare module.exports: typeof axe
 }
